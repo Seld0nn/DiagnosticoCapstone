fb77b9c025e4aa5434fe94b85b3e8e730969225b
Seld0nn <masoto2@uc.cl>
2022-03-26 18:45:15 -0300

    readDB funtion

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hola mundos")
+import re
+import json
+
+def readJson():
+    data = [json.loads(line) for line in open('data.json', 'r')]    
+    return data
+
+def TopTweets():
+    pass
+
+def main():
+    data = readJson()
+    
+main()
